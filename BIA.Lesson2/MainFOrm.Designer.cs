45e850f060d0099db7ec33ddcc3552d49cf7beb1
Jan Vargovský <jan.vargovsky@gmail.com>
2017-09-27 20:59:20 +0200

    Lesson2 complete

---
+++ BIA.Lesson2/MainFOrm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BIA.Lesson2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.functionsComboBox = new System.Windows.Forms.ComboBox();
+            this.renderContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // functionsComboBox
+            // 
+            this.functionsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.functionsComboBox.FormattingEnabled = true;
+            this.functionsComboBox.Location = new System.Drawing.Point(12, 12);
+            this.functionsComboBox.Name = "functionsComboBox";
+            this.functionsComboBox.Size = new System.Drawing.Size(200, 21);
+            this.functionsComboBox.TabIndex = 0;
+            // 
+            // renderContainer
+            // 
+            this.renderContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderContainer.Location = new System.Drawing.Point(12, 39);
+            this.renderContainer.Name = "renderContainer";
+            this.renderContainer.Size = new System.Drawing.Size(760, 510);
+            this.renderContainer.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.renderContainer);
+            this.Controls.Add(this.functionsComboBox);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BIA Lesson 2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox functionsComboBox;
+        private System.Windows.Forms.Panel renderContainer;
+    }
+}
+
---
+++ BIA.Lesson2/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using ILNumerics.Drawing;
+using ILNumerics.Drawing.Plotting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BIA.Lesson2
+{
+    public partial class MainForm : Form
+    {
+        readonly Dictionary<string, TestFunction> functionsDictionary; // <func name, renderFunc>
+        readonly TestFunctions testFunctions;
+
+        readonly ILGroup plotCube;
+        ILSurface surface;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            functionsDictionary = new Dictionary<string, TestFunction>();
+            testFunctions = new TestFunctions();
+
+            InitFunctions();
+
+            plotCube = new ILPlotCube(twoDMode: false);
+            var scene = new ILScene {
+               plotCube,
+            };
+            scene.Camera.Add(new ILCamera());
+
+            renderContainer.Controls.Add(new ILPanel
+            {
+                Scene = scene,
+            });
+
+            functionsComboBox.SelectedValueChanged += FunctionsComboBox_SelectedValueChanged;
+            functionsComboBox.SelectedIndex = 0;
+        }
+
+        void InitFunctions()
+        {
+            void RegisterFunction(string name, TestFunction func)
+            {
+                functionsComboBox.Items.Add(name);
+                functionsDictionary[name] = func;
+            }
+
+            RegisterFunction("Rastrigin function", new TestFunction((x, y) => testFunctions.RastriginFunction(10, x, y), -5.12f, 5.12f));
+            RegisterFunction("Ackley's function", new TestFunction(testFunctions.AckleyFunction, -5f, 5f));
+            RegisterFunction("Sphere function", new TestFunction((x,y) => testFunctions.SphereFunction(x, y), -100, 100));
+            RegisterFunction("Easom function", new TestFunction(testFunctions.EasomFunction, -100, 100));
+            RegisterFunction("Three-hump camel function", new TestFunction(testFunctions.ThreeHumpCamelFunction, -5f, 5f));
+            RegisterFunction("Cross-in-tray function", new TestFunction(testFunctions.CrossInTrayFunction, -10f, 10f));
+            RegisterFunction("Hölder table function", new TestFunction(testFunctions.HolderTableFunction, -10f, 10f));
+            RegisterFunction("McCormick function", new TestFunction(testFunctions.McCormickfunction, -1.5f, 4f, -3f, 4f));
+            RegisterFunction("Schaffer function N. 2", new TestFunction(testFunctions.SchafferFunctionN2, -100f, 100f));
+            RegisterFunction("Schaffer function N. 4", new TestFunction(testFunctions.SchafferFunctionN4, -100f, 100f));
+            RegisterFunction("Matyas function", new TestFunction(testFunctions.MatyasFunction, -10f, 10f));
+            RegisterFunction("Bukin function N.6", new TestFunction(testFunctions.BukinFunctionN6, -15f, -5f, -3f, 3f));
+        }
+
+        void FunctionsComboBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var selectedFunctionValue = (string)functionsComboBox.SelectedItem;
+            var testFunction = functionsDictionary[selectedFunctionValue];
+
+            var surface = new ILSurface(testFunction.Function, 
+                xmin: testFunction.MinX, xmax: testFunction.MaxX, 
+                ymin: testFunction.MinY, ymax: testFunction.MaxY)
+            {
+                Wireframe = { Color = Color.FromArgb(50, Color.LightGray) },
+                Colormap = Colormaps.Jet,
+            };
+
+            if (this.surface != null)
+                plotCube.Remove(this.surface);
+            plotCube.Add(surface);
+            this.surface = surface;
+            renderContainer.Refresh();
+        }
+    }
+}
