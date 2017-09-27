3ba5443c8c2a3a82cb50af3d7ada010eb4016de7
Jan Vargovský <jan.vargovsky@gmail.com>
2017-09-27 17:01:40 +0200

    Custom permutation to MoreLinq.

--- BIA.Lesson1/Program.cs
+++ BIA.Lesson1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -8,62 +9,31 @@ namespace BIA.Lesson1
 {
     class Program
     {
-        class Record
-        {
-            public long TotalTime { get; set; }
-            public int TotalSolutions { get; set; }
-        }
-
         static void Main(string[] args)
         {
-            var results = new Dictionary<int, Record>();
+            var results = new Dictionary<int, (long TotalTime, int TotalSolutions)>();
 
-            for (int i = 2; i <= 11; i++)
+            for (int i = 2; i <= 13; i++)
             {
-                Console.WriteLine("Total nodes " + i);
-
+                Console.WriteLine($"Started {i}");
                 Stopwatch sw = Stopwatch.StartNew();
-
-                var allnodes = Enumerable.Range(0, i + 1)
+                var allnodes = Enumerable.Range(0, i)
                     .Select(t => (char)('a' + t))
                     .ToList();
 
                 var exceptFirst = allnodes.Skip(1).ToList();
-                var permutations = GetPermutations(exceptFirst, exceptFirst.Count)
-                    //.ToList()
-                    ;
-
-                //StringBuilder sb = new StringBuilder();
-                foreach (var nodes in permutations)
-                {
-                    //    string path = string.Format("{0}-{1}-{0}", allnodes[0], string.Join("-", nodes));
-                    //    sb.AppendLine(path);
-                }
-
-                //Console.WriteLine(sb);
+                var permutations = MoreEnumerable.Permutations(exceptFirst);
+                var count = permutations.Count();
 
                 sw.Stop();
-                results[i] = new Record
-                {
-                    TotalSolutions = permutations.Count(),
-                    TotalTime = sw.ElapsedMilliseconds,
-                };
+                results[i] = (sw.ElapsedMilliseconds, count);
 
-                Console.WriteLine("computed in {0}", sw.ElapsedMilliseconds);
+                Console.WriteLine($"Solution count {count}, computed in {sw.ElapsedMilliseconds}ms");
             }
 
             File.WriteAllText("results.csv", string.Join(Environment.NewLine, results.Select(t => string.Format("{0},{1},{2}", t.Key, t.Value.TotalSolutions, t.Value.TotalTime))));
 
             Console.WriteLine("Done");
-        }
-
-        static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-        {
-            if (length == 1) return list.Select(t => new T[] { t });
-
-            return GetPermutations(list, length - 1)
-                .SelectMany(t => list.Where(e => !t.Contains(e)),
-                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
     }
 }
