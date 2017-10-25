180ac623a5bcfc71984d7677e6d86b6df38393e7
Jan Vargovský <jan.vargovsky@gmail.com>
2017-10-25 13:15:29 +0200

    Lesson6

---
+++ BIA.Lesson6/PopulationGenerator.cs
@@ -0,0 +1,51 @@
+﻿using BIA.Shared.TestFunctions;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BIA.Lesson6
+{
+    public class PopulationGenerator
+    {
+        readonly Random random;
+        readonly MinMax<float>[] borders;
+        readonly SimulatedAnnealing simulatedAnnealing;
+
+        public PopulationGenerator(params MinMax<float>[] borders)
+        {
+            random = new Random();
+            this.borders = borders;
+            simulatedAnnealing = new SimulatedAnnealing();
+        }
+
+        float NextFloat((float min, float max) minMax) => (float)random.NextDouble() * (minMax.max - minMax.min) + minMax.min;
+
+        public IEnumerable<float[]> GenerateFirstPopulation(int populationCount)
+        {
+            float[] Generate() =>
+                Enumerable.Range(0, borders.Length)
+                .Select(t => NextFloat(borders[t]))
+                .ToArray();
+
+            while (populationCount-- > 0)
+                yield return Generate();
+        }
+
+        public IEnumerable<float[]> GenerateNextPopulation(IEnumerable<float[]> input, int pointsAroundToGenerate, float range, TestFunctionBase function)
+        {
+            float[] GeneratePoint(float[] p) => simulatedAnnealing.GenerateNextPoint(p, range, function);
+
+            foreach (var item in input)
+            {
+                var newPointsAround = Enumerable.Range(0, pointsAroundToGenerate)
+                    .Select(_ => GeneratePoint(item))
+                    .Select(t => new { Point = t, Fitness = function.Calculate(t) })
+                    .ToArray();
+
+                var min = newPointsAround.MinBy(t => t.Fitness);
+                yield return min.Point;
+            }
+        }
+    }
+}
---
+++ BIA.Lesson6/TabuSearch.cs
@@ -0,0 +1,46 @@
+﻿using BIA.Shared.TestFunctions;
+using System;
+using System.Collections.Generic;
+
+namespace BIA.Lesson6
+{
+    public class TabuSearch
+    {
+        readonly Random random;
+
+        public TabuSearch()
+        {
+            random = new Random();
+        }
+
+        float NextFloat(float min, float max) => (float)random.NextDouble() * (max - min) + min;
+
+        public float[] Search(float[] point, TestFunctionBase testFunction, int iterations, float range)
+        {
+            float[] GeneratePoint(float[] p)
+            {
+                var result = new float[p.Length];
+                for (int i = 0; i < p.Length; i++)
+                    result[i] = NextFloat(p[i] - range / 2, p[i] + range / 2);
+                return result;
+            }
+
+            var tabu = new HashSet<float[]>();
+            var bestPoint = point;
+            for (int i = 0; i < iterations; i++)
+            {
+                float[] newPoint = null;
+                do
+                    newPoint = GeneratePoint(point);
+                while (!tabu.Contains(newPoint));
+
+                tabu.Add(newPoint);
+
+                if (testFunction.Calculate(bestPoint) > testFunction.Calculate(newPoint))
+                    bestPoint = newPoint;
+            }
+
+            return bestPoint;
+        }
+    }
+}
