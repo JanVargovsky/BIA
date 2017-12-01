f2e2196593dad0af97cbf4d4a3ec6d81434b8fbd
Jan Vargovský <jan.vargovsky@gmail.com>
2017-12-01 19:33:10 +0100

    Lesson10 TSP keep better solution on generation.

--- BIA.Lesson10/MainForm.cs
+++ BIA.Lesson10/MainForm.cs
@@ -16,9 +16,13 @@ namespace BIA.Lesson10
 
             btnRunTSP.Click += (o, e) =>
             {
-                tour = tsp.Solve();
-                Text = tour.Distance.ToString();
-                renderContainer.Refresh();
+                var newTour = tsp.Solve();
+                if (newTour.Distance < tour.Distance)
+                {
+                    tour = newTour;
+                    Text = tour.Distance.ToString();
+                    renderContainer.Refresh();
+                }
             };
 
             renderContainer.Paint += (o, e) =>
@@ -26,7 +30,7 @@ namespace BIA.Lesson10
                 var g = e.Graphics;
                 var cities = tour.Cities;
                 foreach (var c in cities)
-                    g.FillRectangle(Brushes.Black, c.X, c.Y, 5, 5);
+                    g.FillRectangle(Brushes.Black, c.X - 2.5f, c.Y - 2.5f, 5, 5);
 
                 void DrawLine(City a, City b) => g.DrawLine(Pens.Black, a.X, a.Y, b.X, b.Y);
 
